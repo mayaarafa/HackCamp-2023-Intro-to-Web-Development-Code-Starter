ee20fcf75d20c3a0cf57509e9ad6f31adbfc0b94
mayaarafa <146292273+mayaarafa@users.noreply.github.com>
2023-11-18 17:11:16 -0800

    Web Development Workshop

---
+++ script.js
@@ -0,0 +1,18 @@
+//alert("Hello, Hackers!")
+console.log("Hello, Hackers!")
+
+let myName = "Nugget";
+console.log(myName);
+
+function say_hello(name) {
+    console.log("Hello, " + name + "!");
+}
+
+say_hello("Maya")
+
+function increment() {
+    score_element = document.getElementById("score");
+    score_element.innerHTML = parseInt(score_element.innerHTML) + 1;
+}
+
+// see slides for how to deploy your website
